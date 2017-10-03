b3a15fc9b14f45a6963402295a4155cce7255229
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-10-03 17:49:38 +0700

    Adding some features on BitHelpers class. Like Binary/Octal/Hexadecimal conversions.

--- MirzaCryptoHelpers/Common/BitHelpers.cs
+++ MirzaCryptoHelpers/Common/BitHelpers.cs
@@ -94,12 +94,14 @@ namespace MirzaCryptoHelpers.Common
         }
         #endregion
 
+
+        
         #region ToBinary Operations
         /// <summary>
-        /// Convert string to binary.
+        /// Convert string input to binary.
         /// </summary>
         /// <param name="input">Input as string.</param>
-        /// <returns>Binary string.</returns>
+        /// <returns>Binary string. Returns Empty string if fails.</returns>
         /// <exception cref="ArgumentNullException">'input' cannot be null/empty.</exception>
         public static string ConvertToBinary(string input)
         {
@@ -119,17 +121,17 @@ namespace MirzaCryptoHelpers.Common
         /// <summary>
         /// Convert char to binary.
         /// </summary>
-        /// <param name="input">Input as char</param>
-        /// <returns>Binary string.</returns>
+        /// <param name="input">Input as char.</param>
+        /// <returns>Binary string. Returns Empty string if fails.</returns>
         public static string ConvertToBinary(char input)
         {
             return ConvertToBinary((long)input);
         }
         /// <summary>
-        /// Convert short to binary
+        /// Convert Int16 input to binary
         /// </summary>
-        /// <param name="input">Input as short</param>
-        /// <returns>Binary string.</returns>
+        /// <param name="input">Input as Int16.</param>
+        /// <returns>Binary string. Returns Empty string if fails.</returns>
         public static string ConvertToBinary(short input)
         {
             string result = "";
@@ -142,10 +144,10 @@ namespace MirzaCryptoHelpers.Common
 
         }
         /// <summary>
-        /// Convert Int32 to binary
+        /// Convert Int32 input to binary
         /// </summary>
-        /// <param name="input">Input as Int32</param>
-        /// <returns>Binary string.</returns>
+        /// <param name="input">Input as Int32.</param>
+        /// <returns>Binary string. Returns Empty string if fails.</returns>
         public static string ConvertToBinary(int input)
         {
             string result = "";
@@ -158,10 +160,10 @@ namespace MirzaCryptoHelpers.Common
 
         }
         /// <summary>
-        /// Convert Int64 to binary
+        /// Convert Int64 input to binary.
         /// </summary>
-        /// <param name="input">Input as Int64</param>
-        /// <returns>Binary string.</returns>
+        /// <param name="input">Input as Int64.</param>
+        /// <returns>Binary string. Returns Empty string if fails.</returns>
         public static string ConvertToBinary(long input)
         {
             string result = "";
@@ -174,12 +176,12 @@ namespace MirzaCryptoHelpers.Common
         }
         #endregion
 
-        #region FromBinaryToInt64
+        #region FromBinary
         /// <summary>
-        /// Convert from binary to array of long. Each binary converted value will be stored in 'result' index.
+        /// Convert from binary to array of Int64. Each binary converted value will be stored in 'result' index.
         /// </summary>
-        /// <param name="binary">Binary data in string format. Ex: 0011 0011</param>
-        /// <param name="result">Array of long. It'll be empty if conversion fails.</param>
+        /// <param name="binary">Binary data in string format. Ex: 0011 0011.</param>
+        /// <param name="result">Array of Int64. It'll be empty if conversion fails.</param>
         /// <returns>True if conversion succeeds. Else returns false.</returns>
         /// <exception cref="ArgumentNullException">'binary' cannot be null/empty</exception>
         public static bool ConvertFromBinary(string binary, out long[] result)
@@ -208,7 +210,7 @@ namespace MirzaCryptoHelpers.Common
         /// <summary>
         /// Convert from binary string to real string.
         /// </summary>
-        /// <param name="binary">Binary data in string format. Ex: 0011 0011</param>
+        /// <param name="binary">Binary data in string format. Ex: 0011 0011.</param>
         /// <param name="result">Real string as a result of conversion.</param>
         /// <returns>True if conversion succeeds. Else returns false.</returns>
         /// <exception cref="ArgumentNullException">'binary' cannot be null/empty</exception>
@@ -233,5 +235,279 @@ namespace MirzaCryptoHelpers.Common
         #endregion
 
 
+
+        #region ToOctal Operations
+        /// <summary>
+        /// Convert string input to octal.
+        /// </summary>
+        /// <param name="input">Input as string.</param>
+        /// <returns>Octal string. Returns empty string if fails.</returns>
+        /// <exception cref="ArgumentNullException">'input' cannot be null/empty.</exception>
+        public static string ConvertToOctal(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException(nameof(input));
+            StringBuilder stringBuilder = new StringBuilder();
+            int len = input.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (i == len - 1)
+                    stringBuilder.Append(ConvertToOctal(input[i]));
+                else
+                    stringBuilder.Append(ConvertToOctal(input[i]) + " ");
+            }
+            return stringBuilder.ToString();
+        }
+        /// <summary>
+        /// Convert char input to octal.
+        /// </summary>
+        /// <param name="input">Input as char.</param>
+        /// <returns>Octal string. Returns empty if fails.</returns>
+        public static string ConvertToOctal(char input)
+        {
+            return ConvertToOctal((long)input);
+        }
+        /// <summary>
+        /// Convert Int16 input to octal.
+        /// </summary>
+        /// <param name="input">Input as Int16.</param>
+        /// <returns>Octal in string. Returns empty if fails.</returns>
+        public static string ConvertToOctal(short input)
+        {
+            
+            string result = ""; ;
+            try
+            {
+                result = Convert.ToString(input, 8);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        /// <summary>
+        /// Convert Int32 input to octal.
+        /// </summary>
+        /// <param name="input">Input as Int32.</param>
+        /// <returns>Octal string. Returns empty if fails.</returns>
+        public static string ConvertToOctal(int input)
+        {
+            string result = "";
+            try
+            {
+                result = Convert.ToString(input, 8);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        /// <summary>
+        /// Convert Int64 to octal.
+        /// </summary>
+        /// <param name="input">Input as Int64.</param>
+        /// <returns>Octal string. Returns empty if fails.</returns>
+        public static string ConvertToOctal(long input)
+        {
+            string result = "";
+            try
+            {
+                result = Convert.ToString(input, 8);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        #endregion
+
+        #region FromOctal Operations
+        /// <summary>
+        /// Convert from octal to array of Int64. Each octal converted value will be stored in 'result' index.
+        /// </summary>
+        /// <param name="octal">Octal data in string format. Ex: 435 014 555.</param>
+        /// <param name="result">Array of Int64. It'll be empty if conversion fails.</param>
+        /// <returns>True if conversion succeeds. Else returns false.</returns>
+        /// <exception cref="ArgumentNullException">'octal' cannot be null/empty.</exception>
+        public static bool ConvertFromOctal(string octal, out long[] result)
+        {
+            if (String.IsNullOrEmpty(octal))
+                throw new ArgumentNullException(nameof(octal));
+            string[] octals = octal.Trim().Split();
+            int len = octals.Length;
+            result = new long[len];
+            for (int i = 0; i < len; i++)
+            {
+                try
+                {
+                    result[i] = Convert.ToInt64(octals[i],8);
+
+                }
+                catch
+                {
+                    result = null;
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Convert from octal string to real string.
+        /// </summary>
+        /// <param name="octal">Octal data in string format. Ex: 435 014 555.</param>
+        /// <param name="result">Real string as a result of conversion.</param>
+        /// <returns>True if conversion succeeds. Else returns false.</returns>
+        /// <exception cref="ArgumentNullException">'octal' cannot be null/empty</exception>
+        public static bool ConvertFromOctal(string octal, out string result)
+        {
+            long[] values = null;
+            bool conversionResult = ConvertFromOctal(octal, out values);
+            result = "";
+            if (!conversionResult)
+                return false;
+            else
+            {
+                foreach (long i in values)
+                {
+                    result += (char)i;
+                }
+
+                return true;
+            }
+
+        }
+        #endregion
+
+
+        
+        #region ToHexadecimal Operations
+        /// <summary>
+        /// Convert string input to hexadecimal.
+        /// </summary>
+        /// <param name="input">Input as string.</param>
+        /// <returns>Hexadecimal string. Returns empty string if fails.</returns>
+        /// <exception cref="ArgumentNullException">'input' cannot be null/empty.</exception>
+        public static string ConvertToHexadecimal(string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException(nameof(input));
+            StringBuilder stringBuilder = new StringBuilder();
+            int len = input.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (i == len - 1)
+                    stringBuilder.Append(ConvertToHexadecimal(input[i]));
+                else
+                    stringBuilder.Append(ConvertToHexadecimal(input[i]) + " ");
+            }
+            return stringBuilder.ToString();
+        }
+        /// <summary>
+        /// Convert char input to hexadecimal.
+        /// </summary>
+        /// <param name="input">Input as char.</param>
+        /// <returns>Hexadecimal string. Returns empty if fails.</returns>
+        public static string ConvertToHexadecimal(char input)
+        {
+            return ConvertToHexadecimal((long)input);
+        }
+        /// <summary>
+        /// Convert Int16 input to hexadecimal.
+        /// </summary>
+        /// <param name="input">Input as Int16.</param>
+        /// <returns>Hexadecimal in string. Returns empty if fails.</returns>
+        public static string ConvertToHexadecimal(short input)
+        {
+
+            string result = ""; 
+            try
+            {
+                result = Convert.ToString(input, 16);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        /// <summary>
+        /// Convert Int32 input to hexadecimal.
+        /// </summary>
+        /// <param name="input">Input as Int32.</param>
+        /// <returns>Hexadecimal string. Returns empty if fails.</returns>
+        public static string ConvertToHexadecimal(int input)
+        {
+            string result = "";
+            try
+            {
+                result = Convert.ToString(input, 16);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        /// <summary>
+        /// Convert Int64 to hexadecimal.
+        /// </summary>
+        /// <param name="input">Input as Int64.</param>
+        /// <returns>Hexadecimal string. Returns empty if fails.</returns>
+        public static string ConvertToHexadecimal(long input)
+        {
+            string result = "";
+            try
+            {
+                result = Convert.ToString(input, 16);
+            }
+            catch { result = ""; }
+            return result;
+        }
+        #endregion
+
+        #region FromHexadecimal Operations
+        /// <summary>
+        /// Convert from hexadecimal to array of Int64. Each hexadecimal converted value will be stored in 'result' index.
+        /// </summary>
+        /// <param name="hexadecimal">Hexadecimal data in string format. Ex: 435 014 555.</param>
+        /// <param name="result">Array of Int64. It'll be empty if conversion fails.</param>
+        /// <returns>True if conversion succeeds. Else returns false.</returns>
+        /// <exception cref="ArgumentNullException">'hexadecimal' cannot be null/empty.</exception>
+        public static bool ConvertFromHexadecimal(string hexadecimal, out long[] result)
+        {
+            if (String.IsNullOrEmpty(hexadecimal))
+                throw new ArgumentNullException(nameof(hexadecimal));
+            string[] hexadecimals = hexadecimal.Trim().Split();
+            int len = hexadecimals.Length;
+            result = new long[len];
+            for (int i = 0; i < len; i++)
+            {
+                try
+                {
+                    result[i] = Convert.ToInt64(hexadecimals[i], 16);
+
+                }
+                catch
+                {
+                    result = null;
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary> 
+        /// Convert from hexadecimal string to real string.
+        /// </summary>
+        /// <param name="hexadecimal">Hexadecimal data in string format. Ex: 1C7A B363 87CD.</param>
+        /// <param name="result">Real string as a result of conversion.</param>
+        /// <returns>True if conversion succeeds. Else returns false.</returns>
+        /// <exception cref="ArgumentNullException">'hexadecimal' cannot be null/empty</exception>
+        public static bool ConvertFromHexadecimal(string hexadecimal, out string result)
+        {
+            long[] values = null;
+            bool conversionResult = ConvertFromHexadecimal(hexadecimal, out values);
+            result = "";
+            if (!conversionResult)
+                return false;
+            else
+            {
+                foreach (long i in values)
+                {
+                    result += (char)i;
+                }
+
+                return true;
+            }
+
+        }
+        #endregion
     }
 }
